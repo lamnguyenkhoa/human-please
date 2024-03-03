e80c211ff93aa8d9918b3e7bc54d13c027ac58d7
NKLam <khoalamvn@gmail.com>
2024-03-03 17:04:26 +1100

    Add DialogArea

---
+++ ui/dialog_area.gd
@@ -0,0 +1,11 @@
+extends Control
+class_name DialogArea
+
+@export var subject_dialog_prefab: PackedScene
+@export var inspector_dialog_prefab: PackedScene
+
+func add_subject_dialog(text: String):
+    return
+
+func add_inspector_dialog(text: String):
+    return
