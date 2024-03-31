a343e49d5f7c2e4d3147d8a8ac72d9dec8802100
NKLam <khoalamvn@gmail.com>
2024-03-31 20:17:45 +1100

    Update guidebook rules depend on day

--- entity/document/guidebook.gd
+++ entity/document/guidebook.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 	await get_tree().process_frame
 	await get_tree().process_frame
 	var day = GameManager.work_day.day
+	rule3_text.visible = false
+	rule4_text.visible = false
+	if day >= 3:
+		rule3_text.visible = true
